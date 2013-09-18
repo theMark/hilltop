01716a2eaa1556ccc3ae6c24cb6d317f002f8bb1
mark <wingman42@gmail.com>
2013-09-17 23:23:43 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hilltop::Application.config.secret_token = 'afa07ab947d948f4f9a79efd716ad10caf17d23b1c5ce980de36723301b424b781c214b39181b88f7a5aeb0af3cf138755e299a0d48a80631c580b87da3f6c86'
